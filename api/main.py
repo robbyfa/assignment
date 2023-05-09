2259e3ed98f5aa88ab9d1f552e9e8972e974d4c9
robertfenechadami <robertfa02@gmail.com>
2023-05-09 10:51:36 +0200

    -

--- api/main.py
+++ api/main.py
@@ -3,11 +3,12 @@ import random
 import os
 from sqlalchemy import create_engine, text
 
-# Get environment variables
-db_user = os.environ.get('DB_USER')
-db_pass = os.environ.get('DB_PASSWORD')
-db_name = os.environ.get('DB_NAME')
-db_socket_dir = os.environ.get('CLOUD_SQL_CONNECTION_NAME')
+
+db_user = "rob"
+db_pass = "uWzKUp8YtnLuRqJP/dbeZLdV"
+db_name = "numbersdb"
+db_socket_dir = "/cloudsql"
+cloud_sql_instance_name = "cis3111-2023-class:europe-west1:db-instance"
 
 # Create database engine
 db_url = f"mysql+pymysql://{db_user}:{db_pass}@/{db_name}?unix_socket={db_socket_dir}"
