328d6f658329fc9322041abce72dab74c6f32db4
robertfenechadami <robertfa02@gmail.com>
2023-05-09 09:33:08 +0200

    updated connection to use SQLAlchemy

--- api/main.py
+++ api/main.py
@@ -1,108 +1,50 @@
-from flask import Flask,render_template,jsonify,request
+from flask import Flask, render_template, jsonify, request
 import random
 import os
-import mysql.connector
-from mysql.connector.constants import ClientFlag
-import mysql.connector
+from sqlalchemy import create_engine, text
 
+# Get environment variables
+db_user = os.environ.get('DB_USER')
+db_pass = os.environ.get('DB_PASSWORD')
+db_name = os.environ.get('DB_NAME')
+db_socket_dir = os.environ.get('CLOUD_SQL_CONNECTION_NAME')
 
-config = {
-    'user': 'rob'
-    'password': ''
-    'host':
-    'client_flags': 
-    'ssl_ca': 
-    'ssl_cert': 
-    'ssl_key': 
-}
-
-# #Setting the database we want to work in
-config['database'] = 'numbersdb'  # add new database to config dict
-
-
-# establish  connection
-connection = mysql.connector.connect(**config)
-cursor = connection.cursor()
-
-
-# Creating a database
-# cursor.execute('CREATE DATABASE numbersdb')
+# Create database engine
+db_url = f"mysql+pymysql://{db_user}:{db_pass}@/{db_name}?unix_socket={db_socket_dir}"
+engine = create_engine(db_url)
 
 # Creating the table
-query1 = ("CREATE TABLE IF NOT EXISTS random_number_table (InstanceName VARCHAR(50), randomNumber int UNSIGNED)")
-cursor.execute(query1)
-
-
-# #Delete all rows in table
-# delete = "DELETE FROM random_number_table"
-# cursor.execute(delete)
-# connection.commit()
-
+with engine.connect() as conn:
+    conn.execute(text("CREATE TABLE IF NOT EXISTS random_number_table (InstanceName VARCHAR(50), randomNumber int UNSIGNED)"))
 
 app = Flask(__name__)
-
 
 @app.route("/")
 def home():
     home_page = 'Home Page'
     return render_template('index.html', home=home_page)
 
-nameList = []
-numList = []
 @app.route('/genNum', methods=['GET', 'POST'])
 def genNumFunc():
-    # #Setting the database we want to work in
-    config['database'] = 'numbersdb'  # add new database to config dict
-    # establish  connection
-    connection = mysql.connector.connect(**config)
-    cursor = connection.cursor()
-
-        # GET request
     if request.method == 'GET':
-            number = random.randint(0, 100000)
-            name = os.environ.get('GAE_INSTANCE')
-            ins = "INSERT INTO random_number_table (InstanceName, randomNumber) VALUES (%s, %s)"
-            values = (name, number)
-            cursor.execute(ins, values)
-            connection.commit()
-            message = {'name': name,'number':number,}
-            return jsonify(message)  # serialize and use JSON headers
-
+        number = random.randint(0, 100000)
+        name = os.environ.get('GAE_INSTANCE')
+        ins = text("INSERT INTO random_number_table (InstanceName, randomNumber) VALUES (:name, :number)")
+        values = {'name': name, 'number': number}
+        with engine.connect() as conn:
+            conn.execute(ins, values)
+        message = {'name': name, 'number': number}
+        return jsonify(message)
 
 @app.route('/db')
 def showdb():
-    # #Setting the database we want to work in
-    config['database'] = 'numbersdb'  # add new database to config dict
-    # establish  connection
-    connection = mysql.connector.connect(**config)
-    cursor = connection.cursor()
+    with engine.connect() as conn:
+        allName = conn.execute(text("SELECT DISTINCT InstanceName FROM random_number_table;")).fetchall()
+        allDetails = conn.execute(text("SELECT * FROM random_number_table")).fetchall()
+        maxNum = conn.execute(text("SELECT * FROM random_number_table WHERE randomNumber = (SELECT MAX(randomNumber) FROM random_number_table);")).fetchall()
+        minNum = conn.execute(text("SELECT * FROM random_number_table WHERE randomNumber = (SELECT MIN(randomNumber) FROM random_number_table);")).fetchall()
 
-
-    query1 = ("SELECT DISTINCT InstanceName FROM random_number_table;")
-    cursor.execute(query1)
-    allName = cursor.fetchall()
-
-    query2 = ("SELECT * FROM random_number_table")
-    cursor.execute(query2)
-    allDetails = cursor.fetchall()
-
-
-    query3 = ("SELECT * FROM random_number_table WHERE randomNumber = (SELECT MAX(randomNumber) FROM random_number_table);")
-    cursor.execute(query3)
-    maxNum = cursor.fetchall()
-
-
-    query4 = ("SELECT * FROM random_number_table WHERE randomNumber = (SELECT MIN(randomNumber) FROM random_number_table);")
-    cursor.execute(query4)
-    minNum = cursor.fetchall()
-
-
-    return render_template('dbans.html',InsName = allName , All = allDetails, Max = maxNum ,Min = minNum)
-
-
-
-
-connection.close()
+    return render_template('dbans.html', InsName=allName, All=allDetails, Max=maxNum, Min=minNum)
 
 if __name__ == '__main__':
-        app.run(host='0.0.0.0', port=8080, debug=False, threaded=True)
+    app.run(host='0.0.0.0', port=8080, debug=False, threaded=True)
